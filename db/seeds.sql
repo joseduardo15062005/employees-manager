a05aa4207bd6194ab209937ede2295049af3c851
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-10-17 08:13:48 -0700

    Add Seeds databases files

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO roles (role_name) VALUES 
+('Web Developer'),
+('Engineering'),
+('Database Engineering');
+
+
+INSERT INTO managers (manager_name) VALUES
+('John Manager'),
+('Johana Manager'),
+('Albert Manager');
+
+
+
+
---
+++ db/seeds_employees.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (first_name, last_name,salary, manager_id,role_id) VALUES 
+('Jose','Velasquez',60000,1,1),
+('Anthony','Huertas',60000,2,1),
+('Sivan','Pending',6000,1,1);
